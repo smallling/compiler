7e581265464fc8fca3afb2ab0c668bf6023b96f1
smallling <lhy526620793@qq.com>
2019-05-09 18:43:09 +0800

    fix codegen.bash

--- codegen.bash
+++ codegen.bash
@@ -5,5 +5,5 @@
 
 set -e
 cd "$(dirname "$0")"
-export CCHK="java -classpath ./lib/antlr-4.7.1-complete.jar:./bin Main"
+export CCHK="java -classpath ./lib/antlr-4.7.2-complete.jar:./bin Main"
 $CCHK
